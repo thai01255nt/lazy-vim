60dfce5614a9038b4c2741ce3acf76ddf35cd8cd
thai <thai0125nt@gmail.com>
2024-07-12 23:45:25 +0700

    feat

--- lua/plugins/extras/lang/on_attach.lua
+++ lua/plugins/extras/lang/on_attach.lua
@@ -1,14 +1,15 @@
 return {
   on_attach = function(client, bufnr)
     vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
-    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
-    vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+    local bufopts = {}
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, vim.tbl_deep_extend("force", {desc: "[g]oto [D]eclarations"}))
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, vim.tbl_deep_extend("force", {desc: "[g]oto [d]efinition"}))
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_deep_extend("force", {desc: "hover"}))
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, vim.tbl_deep_extend("force", {desc: "[g]oto implementation"}))
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, vim.tbl_deep_extend("force", {desc: "signature help"}))
+    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, vim.tbl_deep_extend("force", {desc: "type [D]efinition"}))
+    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, vim.tbl_deep_extend("force", {desc: "[r]en[n]ame variable"}))
+    vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, vim.tbl_deep_extend("force", {desc: "[c]ode [a]ction"}))
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, vim.tbl_deep_extend("force", {desc: "[g]oto [r]eferences"}))
   end,
 }
