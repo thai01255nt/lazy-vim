ecd49239935963c75997c067b4b7be8caa75bd46
thai <thai0125nt@gmail.com>
2024-07-23 03:49:33 +0000

    feature(formatter): add prettier instead jq for json, also using for yaml file

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -44,10 +44,17 @@ keymap.set("n", "<C-w><C-up>", "2<C-W>+")
 keymap.set("n", "<C-w><C-down>", "2<C-W>-")
 
 -- Format json
+-- vim.api.nvim_create_autocmd("BufWritePost", {
+--   pattern = { "*.json" },
+--   callback = function()
+--     vim.cmd("%!jq < %")
+--   end,
+-- })
+-- Format yaml, json
 vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = { "*.json" },
+  pattern = { "*.yaml", "*.yml", "*.json" },
   callback = function()
-    vim.cmd("%!jq < %")
+    vim.cmd("%!prettier --stdin-filepath % --write")
   end,
 })
 -- Diagnostics
