559877d4caf10695c877a459502f644547b6da2a
thai <thai0125nt@gmail.com>
2024-08-07 03:22:59 +0000

    feat(crawler): implement crawler for crawling tracnghiem.com

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,7 +3,8 @@
 -- Add any additional options here
 
 vim.opt.mouse = ""
-
+vim.opt.guicursor =
+  "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 vim.opt.encoding = "utf-8"
 vim.opt.fileencoding = "utf-8"
 
