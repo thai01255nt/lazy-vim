cee751cf390a5f897c74770ea3a53690daa3da89
thai <thai0125nt@gmail.com>
2024-08-11 16:45:02 +0700

    .

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,6 +22,7 @@ require("lazy").setup({
       sv,
     } },
     -- import any extras modules here
+    { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.linting.eslint" },
