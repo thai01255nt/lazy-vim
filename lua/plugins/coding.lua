e482e3ee9609ba71956b846efa41fa9702a6782a
thai <thai0125nt@gmail.com>
2024-10-27 12:09:44 +0700

    feat(coding): remove avante

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -105,76 +105,76 @@ return {
       -- vim.cmd([[cab cc CodeCompanion]])
     end,
   },
-  {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    lazy = false,
-    version = false, -- set this if you want to always pull the latest change
-    opts = {
-      -- add any opts here
-      provider = "claude",
-      auto_suggestion_provider = "claude",
-      hints = { enable = true },
-      behavior = {
-        auto_suggestions = true, -- Experimental stage
-        -- auto_set_highlight_group = true,
-        -- auto_set_keymaps = true,
-        -- auto_apply_diff_after_generation = false,
-        -- support_paste_from_clipboard = false,
-      },
-      claude = {
-        endpoint = "https://api.anthropic.com",
-        model = "claude-3-haiku-20240307",
-        temperature = 0,
-        max_tokens = 4096,
-      },
-    },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      -- "zbirenbaum/copilot.lua", -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-      config = function()
-        require("avante").setup({
-          provider = "claude",
-          auto_suggestions_provider = "claude",
-          hints = { enable = true },
-          behaviour = {
-            auto_suggestions = true,
-          },
-        })
-      end,
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
-    },
-  },
+  -- {
+  --   "yetone/avante.nvim",
+  --   event = "VeryLazy",
+  --   lazy = false,
+  --   version = false, -- set this if you want to always pull the latest change
+  --   opts = {
+  --     -- add any opts here
+  --     provider = "claude",
+  --     auto_suggestion_provider = "claude",
+  --     hints = { enable = true },
+  --     behavior = {
+  --       auto_suggestions = true, -- Experimental stage
+  --       -- auto_set_highlight_group = true,
+  --       -- auto_set_keymaps = true,
+  --       -- auto_apply_diff_after_generation = false,
+  --       -- support_paste_from_clipboard = false,
+  --     },
+  --     claude = {
+  --       endpoint = "https://api.anthropic.com",
+  --       model = "claude-3-haiku-20240307",
+  --       temperature = 0,
+  --       max_tokens = 4096,
+  --     },
+  --   },
+  --   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  --   build = "make",
+  --   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  --   dependencies = {
+  --     "nvim-treesitter/nvim-treesitter",
+  --     "stevearc/dressing.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "MunifTanjim/nui.nvim",
+  --     --- The below dependencies are optional,
+  --     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+  --     -- "zbirenbaum/copilot.lua", -- for providers='copilot'
+  --     {
+  --       -- support for image pasting
+  --       "HakonHarnes/img-clip.nvim",
+  --       event = "VeryLazy",
+  --       opts = {
+  --         -- recommended settings
+  --         default = {
+  --           embed_image_as_base64 = false,
+  --           prompt_for_file_name = false,
+  --           drag_and_drop = {
+  --             insert_mode = true,
+  --           },
+  --           -- required for Windows users
+  --           use_absolute_path = true,
+  --         },
+  --       },
+  --     },
+  --     config = function()
+  --       require("avante").setup({
+  --         provider = "claude",
+  --         auto_suggestions_provider = "claude",
+  --         hints = { enable = true },
+  --         behaviour = {
+  --           auto_suggestions = true,
+  --         },
+  --       })
+  --     end,
+  --     {
+  --       -- Make sure to set this up properly if you have lazy=true
+  --       "MeanderingProgrammer/render-markdown.nvim",
+  --       opts = {
+  --         file_types = { "markdown", "Avante" },
+  --       },
+  --       ft = { "markdown", "Avante" },
+  --     },
+  --   },
+  -- },
 }
