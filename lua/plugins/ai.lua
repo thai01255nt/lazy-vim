9f6e0082f71a65cdaac61f40654fe6657ed8c029
thai0125nt <thai0125nt@gmail.com>
2025-05-21 16:52:38 +0700

    feat: remove redundant code

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,24 +1,3 @@
-local function set_transparency()
-  vim.cmd([[
-    hi Normal guibg=NONE ctermbg=NONE
-    hi NormalNC guibg=NONE ctermbg=NONE
-    hi SignColumn guibg=NONE ctermbg=NONE
-    hi StatusLine guibg=NONE ctermbg=NONE
-    hi StatusLineNC guibg=NONE ctermbg=NONE
-    hi VertSplit guibg=NONE ctermbg=NONE
-    hi TabLine guibg=NONE ctermbg=NONE
-    hi TabLineFill guibg=NONE ctermbg=NONE
-    hi TabLineSel guibg=NONE ctermbg=NONE
-    hi Pmenu guibg=NONE ctermbg=NONE
-    hi PmenuSel guibg=NONE ctermbg=NONE
-    hi NeoTreeNormal guibg=NONE ctermbg=NONE
-    hi NeoTreeNormalNC guibg=NONE ctermbg=NONE
-    hi NeoTreeWinSeparator guibg=NONE ctermbg=NONE
-    hi NeoTreeEndOfBuffer guibg=NONE ctermbg=NONE
-    hi EndOfBuffer guibg=NONE ctermbg=NONE
-  ]])
-end
-
 local IS_DEV = false
 local prompts = {
   -- Code related prompts
