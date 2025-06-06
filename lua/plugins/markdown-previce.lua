af425a9cbc65232fef08d269ff3e0edc891fad3d
thai0125nt <thai0125nt@gmail.com>
2025-06-06 10:35:31 +0700

    feat: update

--- lua/plugins/markdown-previce.lua
+++ lua/plugins/markdown-previce.lua
@@ -27,13 +27,11 @@ return {
   --   end,
   -- },
   {
-    "MeanderingProgrammer/render-markdown.nvim",
-    after = { "nvim-treesitter" },
-    requires = { "echasnovski/mini.nvim", opt = true }, -- if you use the mini.nvim suite
-    requires = { "echasnovski/mini.icons", opt = true }, -- if you use standalone mini plugins
-    -- requires = { 'nvim-tree/nvim-web-devicons', opt = true }, -- if you prefer nvim-web-devicons
-    config = function()
-      require("render-markdown").setup({})
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
     end,
   },
   {
