5d5ec45a489155b5342912d0b0d67c2a83f76442
thai <thai0125nt@gmail.com>
2026-02-11 22:12:19 +0700

    feat: update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -50,25 +50,4 @@ vim.g.lazyvim_picker = "telescope"
 vim.g.snacks_animate = false
 vim.g.lazyvim_cmp = "nvim-cmp"
 
-vim.opt.clipboard = "unnamedplus"
--- Clipboard
-if vim.env.SSH_TTY then
-  opt.clipboard:append("unnamedplus")
-  local function paste(_)
-    return function()
-      local content = vim.fn.getreg('"')
-      return vim.split(content, "\n")
-    end
-  end
-  vim.g.clipboard = {
-    name = "OSC 52",
-    copy = {
-      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-    },
-    paste = {
-      ["+"] = paste("+"),
-      ["*"] = paste("*"),
-    },
-  }
-end
+vim.opt.clipboard:append("unnamedplus")
