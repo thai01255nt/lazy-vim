627bdd1516673708b7d511059267c1d93d858d9d
thai0125nt <thai0125nt@gmail.com>
2026-03-06 12:05:44 +0700

    feat: update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -50,4 +50,13 @@ vim.g.lazyvim_picker = "telescope"
 vim.g.snacks_animate = false
 vim.g.lazyvim_cmp = "nvim-cmp"
 
-vim.opt.clipboard:append("unnamedplus")
+vim.o.clipboard = "unnamedplus"
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    -- vim.highlight.on_yank()
+    local copy_to_unnamedplus = require("vim.ui.clipboard.osc52").copy("+")
+    copy_to_unnamedplus(vim.v.event.regcontents)
+    local copy_to_unnamed = require("vim.ui.clipboard.osc52").copy("*")
+    copy_to_unnamed(vim.v.event.regcontents)
+  end,
+})
